1abf582738e25bb1fe8c608ec70096140f20bb2c
CHThia <capitoljazzpro@gmail.com>
2024-09-11 21:07:38 +0800

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -5,7 +5,7 @@ export default function App() {
       <h1>Welcome to my online Portfolio</h1>
       <hr/>
       <p>My portfolio website is currently in development but you can visit me at my <a href="https://www.linkedin.com/in/thiach/" target="_blank">linkedin</a> profile.</p>
-      <p>Thank you!!</p>
+      <p>Thank you for your patience!!</p>
     </>
   )
 }

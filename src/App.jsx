49c888a1cf0b439e118482e2328d983d123e4b08
CHThia <capitoljazzpro@gmail.com>
2024-09-11 19:41:06 +0800

    Remove Tailwind and Postcss

--- src/App.jsx
+++ src/App.jsx
@@ -3,17 +3,10 @@ export default function App() {
 
   return (
     <>
-      <h1 className="text-center text-[35px] font-bold font-sans mt-[20px] mb-[20px] mx-auto">
-        Welcome to my online Portfolio
-      </h1>
-
+      <h1>Welcome to my online Portfolio</h1>
       <hr/>
-
-      <p className="text-center text-[25px] font-sans mt-[50px] mx-auto">
-        My portfolio website is currently in development but you can visit me at my <a href="https://www.linkedin.com/in/thiach/" target="_blank" className="underline text-blue-600">linkedin</a> profile.
-      </p>
-      
-      <p className="text-center text-[25px] font-sans mt-[20px] mb-[20px] mx-auto" >Thank you!!</p>
+      <p>My portfolio website is currently in development but you can visit me at my <a href="https://www.linkedin.com/in/thiach/" target="_blank">linkedin</a> profile.</p>
+      <p>Thank you!!</p>
     </>
   )
 }

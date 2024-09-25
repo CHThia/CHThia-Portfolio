8807401ace8198dfdf07fea25748b698375ae337
CHThia <capitoljazzpro@gmail.com>
2024-09-25 20:37:51 +0800

    Fixed Deployment Error

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,4 +1,4 @@
-import PersonalProfile from '../img/caricatureProfile.PNG'
+import PersonalProfile from '../img/CaricatureProfile.png'
 
 export default function homePage() {
 
@@ -66,9 +66,3 @@ export default function homePage() {
     </>
   )
 }
-
-
-
-{/* <h1 style={{ marginBottom: '40px', fontWeight: 'bold' }}>Welcome To My Software Engineering Portfolio</h1>
-<p>My portfolio website is currently in development but you can visit me at my <a href="https://www.linkedin.com/in/thiach/" target="_blank">linkedin</a> profile.</p>
-<p>Thank you for your patience!!</p> */}

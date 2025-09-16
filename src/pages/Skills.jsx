20ace03567b7c50e742bfb5decff513823120086
CHThia <capitoljazzpro@gmail.com>
2025-09-16 15:58:57 +0800

    Update Technology Skill Showcase

--- src/pages/Skills.jsx
+++ src/pages/Skills.jsx
@@ -4,15 +4,15 @@ import Js from '../img/JavaScript-Logo.png'
 import ReactJS from '../img/React-Logo.png'
 import Node from '../img/Node-Logo.png'
 import Express from '../img/Express-Logo.png'
-import MongoDB from '../img/MongoDB-Logo.png'
 import PostgreSQL from '../img/PostgreSQL-Logo.png'
 import Konva from '../img/Konva-Logo.png'
-import Laravel from '../img/Laravel-Logo.png'
-import Php from '../img/Php-Logo.png'
 import MySQL from '../img/MySQL-Logo.png'
 import BootStrap from '../img/BootStrap-Logo.png'
 import Xampp from '../img/XAMPP-Logo.png'
 import Git from '../img/Github-Logo.png'
+// import MongoDB from '../img/MongoDB-Logo.png'
+// import Laravel from '../img/Laravel-Logo.png'
+// import Php from '../img/Php-Logo.png'
 
 
 export default function skill () {
@@ -65,16 +65,6 @@ export default function skill () {
           </div>
 
           <div>
-            <img src= { BootStrap } alt="html" style={{ maxWidth: '100px', height: 'auto', display: 'block' }}/>
-            <p style= {{margin: '8px 0 0 5px', fontSize:'20px',  fontFamily:'roboto, arial' }}>BootStrap</p>
-          </div>
-
-          <div>
-            <img src= { Git } alt="html" style={{ maxWidth: '100px', height: 'auto', display: 'block' }}/>
-            <p style= {{margin: '8px 0 0 5px', fontSize:'20px',  fontFamily:'roboto, arial' }}>GitHub</p>
-          </div>
-
-          <div>
             <img src= { ReactJS } alt="html" style={{ maxWidth: '100px', height: 'auto', display: 'block' }}/>
             <p style= {{margin: '8px 0 0 5px', fontSize:'20px',  fontFamily:'roboto, arial' }}>React </p>
           </div>
@@ -89,40 +79,52 @@ export default function skill () {
             <p style= {{margin: '8px 0 0 5px', fontSize:'20px',  fontFamily:'roboto, arial' }}>Express</p>
           </div>
 
-          <div>
-            <img src= { MongoDB } alt="html" style={{ maxWidth: '100px', height: 'auto', display: 'block' }}/>
-            <p style= {{margin: '8px 0 0 5px', fontSize:'20px',  fontFamily:'roboto, arial' }}>MongoDB</p>
-          </div>
 
           <div>
             <img src= { PostgreSQL } alt="html" style={{ maxWidth: '100px', height: 'auto', display: 'block' }}/>
             <p style= {{margin: '8px 0 0 5px', fontSize:'20px',  fontFamily:'roboto, arial' }}>PostgreSQL</p>
           </div>
 
+
+          <div>
+            <img src= { MySQL } alt="html" style={{ maxWidth: '100px', height: 'auto', display: 'block' }}/>
+            <p style= {{margin: '8px 0 0 5px', fontSize:'20px',  fontFamily:'roboto, arial' }}>MySQL</p>
+          </div>
+          
           <div>
             <img src= { Konva } alt="html" style={{ maxWidth: '100px', height: 'auto', display: 'block' }}/>
             <p style= {{margin: '8px 0 0 5px', fontSize:'20px',  fontFamily:'roboto, arial' }}>Konva JS</p>
           </div>
 
           <div>
-            <img src= { Laravel } alt="html" style={{ maxWidth: '100px', height: 'auto', display: 'block' }}/>
-            <p style= {{margin: '8px 0 0 5px', fontSize:'20px',  fontFamily:'roboto, arial' }}>Laravel</p>
+            <img src= { BootStrap } alt="html" style={{ maxWidth: '100px', height: 'auto', display: 'block' }}/>
+            <p style= {{margin: '8px 0 0 5px', fontSize:'20px',  fontFamily:'roboto, arial' }}>BootStrap</p>
           </div>
 
           <div>
-            <img src= { Php } alt="html" style={{ maxWidth: '100px', height: 'auto', display: 'block' }}/>
-            <p style= {{margin: '8px 0 0 5px', fontSize:'20px',  fontFamily:'roboto, arial' }}>PHP</p>
-          </div>
-
-          <div>
-            <img src= { MySQL } alt="html" style={{ maxWidth: '100px', height: 'auto', display: 'block' }}/>
-            <p style= {{margin: '8px 0 0 5px', fontSize:'20px',  fontFamily:'roboto, arial' }}>MySQL</p>
+            <img src= { Git } alt="html" style={{ maxWidth: '100px', height: 'auto', display: 'block' }}/>
+            <p style= {{margin: '8px 0 0 5px', fontSize:'20px',  fontFamily:'roboto, arial' }}>GitHub</p>
           </div>
 
           <div>
             <img src= { Xampp } alt="html" style={{ maxWidth: '100px', height: 'auto', display: 'block' }}/>
             <p style= {{margin: '8px 0 0 5px', fontSize:'20px',  fontFamily:'roboto, arial' }}>XAMPP</p>
           </div>
+
+          {/* <div>
+            <img src= { MongoDB } alt="html" style={{ maxWidth: '100px', height: 'auto', display: 'block' }}/>
+            <p style= {{margin: '8px 0 0 5px', fontSize:'20px',  fontFamily:'roboto, arial' }}>MongoDB</p>
+          </div> */}
+
+          {/* <div>
+            <img src= { Laravel } alt="html" style={{ maxWidth: '100px', height: 'auto', display: 'block' }}/>
+            <p style= {{margin: '8px 0 0 5px', fontSize:'20px',  fontFamily:'roboto, arial' }}>Laravel</p>
+            </div> */}
+
+          {/* <div>
+            <img src= { Php } alt="html" style={{ maxWidth: '100px', height: 'auto', display: 'block' }}/>
+            <p style= {{margin: '8px 0 0 5px', fontSize:'20px',  fontFamily:'roboto, arial' }}>PHP</p>
+            </div> */}
 
         </div>
       </div>

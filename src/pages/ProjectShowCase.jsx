b7d1fc55ba13335fd31549fc1001299238fd9762
CHThia <capitoljazzpro@gmail.com>
2025-09-16 16:00:13 +0800

    Update Project Showcase

--- src/pages/ProjectShowCase.jsx
+++ src/pages/ProjectShowCase.jsx
@@ -1,8 +1,8 @@
 import ProjectOne from '../components/Project_01'
 import ProjectTwo from '../components/Project_02'
 import ProjectThree from '../components/Project_03'
-import ProjectFour from '../components/Project_04'
-import ProjectFive from '../components/Project_05'
+// import ProjectFour from '../components/Project_04'
+// import ProjectFive from '../components/Project_05'
 
 
 export default function ProjectShowcase() {
@@ -41,8 +41,8 @@ export default function ProjectShowcase() {
           <ProjectOne />
           <ProjectTwo />
           <ProjectThree />
-          <ProjectFive />
-          <ProjectFour />
+          {/* <ProjectFive />
+          <ProjectFour /> */}
         </div>
      </div>
     </div>
